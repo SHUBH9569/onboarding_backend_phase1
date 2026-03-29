b250dd76aad85842b1f4ef1cdd8d977724d55f67
Shubh Tiwari <shubhtiwari9569@gmail.com>
2026-03-29 15:01:34 +0530

    update env

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ async function startServer() {
   await mongoose.connect(process.env.MONGO_URI);
   console.log(" MongoDB connected");
 
-  app.listen(4000, () => {
+  app.listen(process.env.PORT, () => {
     console.log(`Server ready at http://localhost:4000/graphql`);
   });
 }
